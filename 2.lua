4f721475d915c811b97519086029d23cdfdca877
ledinhchinh0212 <khongbachofficial@gmail.com>
2024-05-22 11:49:25 +0700

    complete

--- 2.lua
+++ 2.lua
@@ -1,2 +1,16 @@
 local name = "Chinh"
-print("Hello everyone, my name is " .. name)
+local ho = "Le Dinh"
+local fullName = ho .. " " .. name
+print("Hello everyone, my name is " .. fullName)
+
+local des = [[
+    name
+    ho
+    fullName
+]]
+
+C = 20
+local a = 10
+print(C, a)
+
+_G.Hello = 1
