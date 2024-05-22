31fb2ab932d22924bfbdf664451197639c6d2029
ledinhchinh0212 <khongbachofficial@gmail.com>
2024-05-23 00:19:48 +0700

    complete

---
+++ 10.lua
@@ -0,0 +1,3 @@
+local mod = require("mymath")
+print(mod.adds(3, 4))
+print(mod.mpower(3, 4))
---
+++ mymath.lua
@@ -0,0 +1,11 @@
+_G.mmath = {}
+
+function mmath.adds(x, y)
+    return x + y
+end
+
+function mmath.mpower(x, y)
+    return x ^ y
+end
+
+return mmath
